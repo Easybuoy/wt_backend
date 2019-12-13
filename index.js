2cd88dfee88975f6b3d0eded1dea911c0904984c
Modurotolu Olokode <durolawk@gmail.com>
2019-12-13 13:34:05 +0100

    set up server on local host

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const server = require('./api/server');
 require('dotenv').config();
+
+const port = process.nextTick.PORT || 5000;
+server.listen(port, () => {
+    console.log(`\n=== Server listening on port ${port} ===\n`)
+});
