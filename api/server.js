3d1fb7aeba73fc9f8099e2a30d108fcc862e3faf
Melquisedeque Pereira <melqui1@live.com.pt>
2019-12-16 16:24:52 +0000

    Connect server with mongo database and set up graphql

--- api/server.js
+++ api/server.js
@@ -2,14 +2,25 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const bodyParser = require('body-parser');
+const graphQLHttp = require('express-graphql');
+
+const graphQLSchema = require('../graphql/schema/index');
+const graphQLResolvers = require('../graphql/resolvers/index');
+const isLoggedIn = require('../middleware/isLoggedIn');
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
-server.use(express.json());
+server.use(bodyParser.json());
 
-server.get('/', (req, res) => {
-    res.send({up: `workout or stay-out!!!`})
-});
+server.use(isLoggedIn);
+
+server.use('/api', graphQLHttp({
+  schema: graphQLSchema,
+  rootValue: graphQLResolvers,
+  graphiql: true
+}));
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const server = require('./api/server');
 require('dotenv').config();
+const mongoose = require('mongoose');
+const server = require('./api/server');
 
-const port = process.nextTick.PORT || 5000;
-server.listen(port, () => {
-    console.log(`\n=== Server listening on port ${port} ===\n`)
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+).then(() => {
+  console.log(`Successfully connected to the database!`)
+  server.listen(process.env.PORT, (req, res, next) => {
+    console.log(`Successfully connected to localhost:${process.env.PORT}`);
+  });
+}).catch(err => {
+  console.log(err);
 });
