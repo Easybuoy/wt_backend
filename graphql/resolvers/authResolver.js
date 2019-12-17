6426e50f1ecfc6e44ad97010eb4814bd4ec38385
Melquisedeque Pereira <melqui1@live.com.pt>
2019-12-17 19:06:52 +0000

    Added resolvers for login on multiple platforms

--- graphql/resolvers/authResolver.js
+++ graphql/resolvers/authResolver.js
@@ -1,52 +1,109 @@
 const config = require('../../config');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const passport = require('passport');
+const { authenticateFacebook, authenticateGoogle } = require('../../middleware/authentication');
 
 const User = require('../../models/user');
 
+const genAuthenticationResponse = (user) => {
+  return {
+    id: user.id,
+    name: user.name,
+    token: user.generateJWT(),
+  }
+}
+
 module.exports = {
-  createUser: async ({ input }) => {
-    try {
-      const userAlreadyExists = await User.findOne({ email: input.email });
-      if (userAlreadyExists) {
-        throw new Error('User exists already.');
+  Query: {
+    // Login with a normal form
+    authForm: async (_, { input: { email, password } }, { req, res }) => {
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        throw new Error('User does not exist!');
       }
-      const hashedPassword = await bcrypt.hash(input.password, 12);
-
-      const user = new User({
-        email: input.email,
-        password: hashedPassword,
-        type: input.type,
-      });
-
-      const savedUser = await user.save();
-
-      return { ...savedUser._doc, password: null, _id: savedUser.id };
-    } catch (err) {
-      throw err;
-    }
+      if (!user.validPassword(password)) {
+        throw new Error('Password is incorrect!');
+      }
+      return genAuthenticationResponse(user);
+    },
   },
-  login: async ({ input }, context, info) => {
-    const user = await User.findOne({ email: input.email });
-    if (!user) {
-      throw new Error('User does not exist!');
-    }
-    const isEqual = await bcrypt.compare(input.password, user.password);
-    if (!isEqual) {
-      throw new Error('Password is incorrect!');
-    }
-    const token = jwt.sign(
-      { 
-        type: user.type, 
-        userId: user.id, 
-        email: user.email
-      },
-      config.jwtSecret,
-      { 
-        expiresIn: '1h'
+  Mutation: {
+    // Sign up with a normal form
+    addUser: async (_, { input }, { req, res }) => {
+      try {
+        const userAlreadyExists = await User.findOne({ email: input.email });
+        if (userAlreadyExists) {
+          throw new Error('User exists already.');
+        }
+
+        const user = new User({
+          ...input
+        });
+
+        const savedUser = await user.save();
+
+        return { ...savedUser._doc, password: null, _id: savedUser.id };
+      } catch (err) {
+        throw err;
       }
-    );
-    return { id: user.id, token: token, tokenExpiration: 1 };
+    },
+    // Sign up / Login with a facebook account
+    authFacebook: async (_, { input: { accessToken } }, { req, res }) => {
+      req.body = {
+        ...req.body,
+        access_token: accessToken,
+      };
+      try {
+        // data contains the accessToken, refreshToken and profile from passport
+        const { data, info } = await authenticateFacebook(req, res);
+        if (data) {
+          const user = await User.upsertFbUser(data);
+          if (user) {
+            return genAuthenticationResponse(user);
+          }
+        }
+        if (info) {
+          console.log(info);
+          switch (info.code) {
+            case 'ETIMEDOUT':
+              return (new Error('Failed to reach Facebook: Try Again'));
+            default:
+              return (new Error('Something went wrong while logging in with your account!'));
+          }
+        }
+        return (new Error('Server error'));
+      } catch (error) {
+        return error;
+      }
+    },
+    // Sign up / Login with a google account
+    authGoogle: async (_, { input: { accessToken } }, { req, res }) => {
+      req.body = {
+        ...req.body,
+        access_token: accessToken,
+      };
+      try {
+        // data contains the accessToken, refreshToken and profile from passport
+        const { data, info } = await authenticateGoogle(req, res);
+        if (data) {
+          const user = await User.upsertGoogleUser(data);
+          if (user) {
+            return genAuthenticationResponse(user);
+          }
+        }
+        if (info) {
+          console.log(info);
+          switch (info.code) {
+            case 'ETIMEDOUT':
+              return (new Error('Failed to reach Google: Try Again'));
+            default:
+              return (new Error('Something went wrong while logging in with your account!'));
+          }
+        }
+        return (new Error('Server error'));
+      } catch (error) {
+        return error;
+      }
+    },
   }
 };
