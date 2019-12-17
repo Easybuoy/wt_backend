2af5326aff4af8f51b443d52e52879a5794b31ac
Melquisedeque Pereira <melqui1@live.com.pt>
2019-12-17 19:09:31 +0000

    Added authentication for facebook and google with passport

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,69 +1,49 @@
 const config = require('../config');
-const express = require('express');
-const passport = require('passport');
-const { ExtractJwt } = require('passport-jwt');
-const JwtStrategy = require('passport-jwt').Strategy;
-const LocalStrategy = require('passport-local');
-const GoogleStrategy = require('passport-google').Strategy;
-const FacebookStrategy = require('passport-facebook');
-
 const User = require('../models/user');
+const passport = require('passport');
+const FacebookTokenStrategy = require('passport-facebook-token');
+const { Strategy: GoogleTokenStrategy } = require('passport-google-token');
 
-passport.use('jwt', new JwtStrategy({
-  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-  secretOrKey: config.jwtSecret
-}, async (payload, done) => {
-  try {
-    const user = await User.findById(payload.userId);
+const FacebookTokenStrategyCallback = (accessToken, refreshToken, profile, done) => done(null, {
+  accessToken,
+  refreshToken,
+  profile,
+});
 
-    if (!user) {
-      return done(null, false);
-    }
-
-    done(null, user);
-  } catch (error) {
-    done(error, false);
-  }
-}));
-
-passport.use('google', new GoogleStrategy(
-  {
-    returnURL: 'http://localhost:3000/auth/google/return',
-    realm: 'http://localhost:3000/'
-  },
-  async (identifier, done) => {
-    await User.findOrCreate({ googleId: identifier }, (err, user) => {
-      return done(err, user);
-    });
-  }
-));
-
-passport.use('facebook', new FacebookStrategy(
+passport.use(new FacebookTokenStrategy(
   {
     clientID: config.facebookAppId,
     clientSecret: config.facebookAppSecret,
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
   },
-  async (accessToken, refreshToken, profile, cb) => {
-    await User.findOrCreate({ facebookId: profile.id }, (err, user) => {
-      return cb(err, user);
-    });
-  }
+  FacebookTokenStrategyCallback
 ));
 
-passport.serializeUser((user, done) => {
-  console.log('serializedUser', user)
-  done(null, user.id);
+const GoogleTokenStrategyCallback = (accessToken, refreshToken, profile, done) => done(null, {
+  accessToken,
+  refreshToken,
+  profile,
 });
 
-passport.deserializeUser((id, done) => {
-  console.log('deserializedUser', id)
-  User.findById(id, (err, user) => {
-    done(err, user);
-  });
+passport.use(new GoogleTokenStrategy(
+  {
+    clientID: config.googleAppId,
+    clientSecret: config.googleAppSecret,
+  },
+  GoogleTokenStrategyCallback
+));
+
+const authenticateFacebook = (req, res) => new Promise((resolve, reject) => {
+  passport.authenticate('facebook-token', { session: false }, (err, data, info) => {
+    if (err) reject(err);
+    resolve({ data, info });
+  })(req, res);
 });
 
-const authMiddleware = express();
-authMiddleware.use(passport.initialize());
-authMiddleware.use(passport.session());
-module.exports = authMiddleware;
+const authenticateGoogle = (req, res) => new Promise((resolve, reject) => {
+  passport.authenticate('google-token', { session: false }, (err, data, info) => {
+    if (err) reject(err);
+    resolve({ data, info });
+  })(req, res);
+});
+
+module.exports = { authenticateFacebook, authenticateGoogle };
