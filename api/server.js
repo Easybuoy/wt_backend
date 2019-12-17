6622f3f0d701abfcc542b075116a64ca03d45207
Melquisedeque Pereira <melqui1@live.com.pt>
2019-12-17 19:02:53 +0000

    Updated express server to use Apollo server

--- api/server.js
+++ api/server.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const authMiddleware = require('../middleware/authentication');
-const graphQLHttp = require('../middleware/graphql');
+const typeDefs = require('../graphql/schema');
+const resolvers = require('../graphql/resolvers');
 
-const server = express();
+const { ApolloServer } = require('apollo-server-express');
 
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
+const app = express();
 
-server.use(authMiddleware);
-server.use('/api', graphQLHttp);
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
-server.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send({ up: `workout or stay-out!!!` })
 });
 
-module.exports = server;
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,  
+  context: ({ req, res }) => ({ req, res }), // adds request and response to graphQL context
+});
+
+apolloServer.applyMiddleware({ app, path: '/api' });
+
+module.exports = app;
