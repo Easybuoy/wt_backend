a6556b3b462e261f1bac7200d8d391e91d76bd59
Melquisedeque Pereira <melqui1@live.com.pt>
2019-12-17 13:03:28 +0000

    Start using passport

--- graphql/resolvers/authResolver.js
+++ graphql/resolvers/authResolver.js
@@ -1,6 +1,33 @@
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 const User = require('../../models/user');
+
+passport.use(new LocalStrategy((username, password, done) => {
+    User.findOne({ email: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+      if (!user.verifyPassword(password)) {
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
 
 module.exports = {
   createUser: async ({ input }) => {
@@ -23,5 +50,8 @@ module.exports = {
       throw err;
     }
   },
-  
+  login: async () => {
+
+    return {}
+  }
 };
