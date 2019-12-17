b37df2821b1109107f55c8d0c6b885702e0a71e4
Melquisedeque Pereira <melqui1@live.com.pt>
2019-12-17 19:04:17 +0000

    Updated server settings to fetch from the config file

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-const {
-  port,
-  mongoUser,
-  mongoPassword,
-  mongoCluster,
-  mongoDatabase
-} = require('./config');
+const config = require('./config');
 const mongoose = require('mongoose');
 const server = require('./api/server');
 
 mongoose.connect(
-  `mongodb+srv://${mongoUser}:${mongoPassword}@${mongoCluster}/${mongoDatabase}?retryWrites=true`,
+  config.mongoConnect,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }
 ).then(() => {
   console.log(`Successfully connected to the database!`)
-  server.listen(port, (req, res, next) => {
-    console.log(`Successfully connected to localhost:${port}`);
+  server.listen(config.port, (req, res, next) => {
+    console.log(`Successfully connected to localhost:${config.port}`);
   });
 }).catch(err => {
   console.log(err);
