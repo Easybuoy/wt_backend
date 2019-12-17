edc0a70a3a057e99d080e7b20e7cc34972bd2a8f
Melquisedeque Pereira <melqui1@live.com.pt>
2019-12-17 09:05:55 +0000

    Removed unnecessary body-parser package

--- api/server.js
+++ api/server.js
@@ -2,20 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-const bodyParser = require('body-parser');
 const graphQLHttp = require('express-graphql');
 
 const graphQLSchema = require('../graphql/schema/index');
 const graphQLResolvers = require('../graphql/resolvers/index');
-const isLoggedIn = require('../middleware/isLoggedIn');
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
-server.use(bodyParser.json());
-
-server.use(isLoggedIn);
+server.use(express.json());
 
 server.use('/api', graphQLHttp({
   schema: graphQLSchema,
