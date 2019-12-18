2f152156ddd5d4723253a599554e7476ab9d07cb
Leelsmuth <femianjorin@rocketmail.com>
2019-12-18 16:01:18 +0100

    created authentication.js, setup auth middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,48 +1,20 @@
-const passport = require('passport');
-const FacebookTokenStrategy = require('passport-facebook-token');
-const { Strategy: GoogleTokenStrategy } = require('passport-google-token');
+const jwt = require('jsonwebtoken');
 const config = require('../config');
 
-const FacebookTokenStrategyCallback = (accessToken, refreshToken, profile, done) => done(null, {
-  accessToken,
-  refreshToken,
-  profile,
-});
-
-passport.use(new FacebookTokenStrategy(
-  {
-    clientID: config.facebookAppId,
-    clientSecret: config.facebookAppSecret,
-  },
-  FacebookTokenStrategyCallback
-));
-
-const GoogleTokenStrategyCallback = (accessToken, refreshToken, profile, done) => done(null, {
-  accessToken,
-  refreshToken,
-  profile,
-});
-
-passport.use(new GoogleTokenStrategy(
-  {
-    clientID: config.googleAppId,
-    clientSecret: config.googleAppSecret,
-  },
-  GoogleTokenStrategyCallback
-));
-
-const authenticateFacebook = (req, res) => new Promise((resolve, reject) => {
-  passport.authenticate('facebook-token', { session: false }, (err, data, info) => {
-    if (err) reject(err);
-    resolve({ data, info });
-  })(req, res);
-});
-
-const authenticateGoogle = (req, res) => new Promise((resolve, reject) => {
-  passport.authenticate('google-token', { session: false }, (err, data, info) => {
-    if (err) reject(err);
-    resolve({ data, info });
-  })(req, res);
-});
-
-module.exports = { authenticateFacebook, authenticateGoogle };
+module.exports = (req, res, next) => {
+  const token = req.authorization;
+  if (!token) {
+    req.isAuth = false;
+    return next();
+  }
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, config.jwtSecret);
+    req.isAuth = true;
+    req.userId = decodedToken.id;
+    return next();
+  } catch (err) {
+    req.isAuth = false;
+    return next();
+  }
+};
