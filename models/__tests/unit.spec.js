dda9bef761571798b7b1e309bcae1d7ff0c7a871
Sandrava Philips <lacasandra1@gmail.com>
2019-12-20 13:14:27 +0100

    wrote tests for the unit model

---
+++ models/__tests/unit.spec.js
@@ -0,0 +1,14 @@
+const { initTest } = require('../../helpers/tests');
+const Unit = require('../unit');
+
+initTest();
+
+describe('Unit', () => {
+  it('it should be able to save an entry to the table', async (done) => {
+    const unit = new Unit({ name: 'kg' });
+    await unit.save();
+    const units = await Unit.find();
+    done();
+    expect(units).toHaveLength(1);
+  });
+});
