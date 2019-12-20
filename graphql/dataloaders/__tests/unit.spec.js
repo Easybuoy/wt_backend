a768b04ce78fa265ca44f58b9bf97a089b666ba1
Melquisedeque Pereira <melqui1@live.com.pt>
2019-12-20 11:03:04 +0000

    Added base tests for unit dataloader and passport middleware

---
+++ graphql/dataloaders/__tests/unit.spec.js
@@ -0,0 +1,9 @@
+const { initTest, query } = require('../../../helpers/tests');
+
+initTest();
+
+describe('Unit data loader', () => {
+  it('Should only make one request for units', async (done) => {
+    done();
+  });
+});
