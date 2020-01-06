cad50e3ef3b872bc13bd006e6b112e937b0901f8
Modurotolu Olokode <durolawk@gmail.com>
2020-01-06 17:11:03 +0100

    built workout schema

--- models/workout.js
+++ models/workout.js
@@ -5,23 +5,23 @@ mongoose.promise = global.Promise;
 const { Schema } = mongoose;
 
 const WorkoutSchema = new Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-    },
-    name: {
-        type: String,
-        require: true,
-    },
-    description: {
-        type: String,
-    },
-    intensity: {
-        type: String,
-    },
-    type: {
-        type: String,
-    }
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+  },
+  intensity: {
+    type: String,
+  },
+  type: {
+    type: String,
+  }
 });
 
 module.exports = mongoose.model('Workout', WorkoutSchema);
