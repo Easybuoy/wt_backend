6b93684ceb73352b918803e08bb2d2d3cd566884
Melquisedeque Pereira <melqui1@live.com.pt>
2020-01-07 17:31:00 +0000

    Added seed info for workout and workout exercises

--- helpers/seed.js
+++ helpers/seed.js
@@ -5,6 +5,8 @@ const { isProduction } = require('../config');
 const User = require('../models/user');
 const Unit = require('../models/unit');
 const Exercise = require('../models/exercise');
+const Workout = require('../models/workout');
+const WorkoutExercise = require('../models/workoutExercise');
 
 module.exports = async (onEnd = false) => {
   console.log('Connecting to database...');
@@ -43,6 +45,17 @@ module.exports = async (onEnd = false) => {
   // eslint-disable-next-line global-require
   const exercises = require('./exercise');
 
+  const workouts = [
+    {
+      userId: null,
+      name: 'Chest and Shoulder Smackdown',
+      description: 'Use this in place of your regular chest-and-delt workout if you\'re in lean-out mode. While it\'s more like a standard muscle-building training routine, you\'ll rev up the fat-burn a bit when you keep your rest periods under 30 seconds.',
+      picture: 'https://www.bodybuilding.com/images/2018/april/5-workous-that-are-insanely-efficient-at-torching-fat-signature-4-700xh.jpg',
+      intensity: '',
+      type: '',
+    }
+  ];
+
   if (!isProduction) {
     console.log('Seeding users collection...');
     await User.create(users);
@@ -63,9 +76,33 @@ module.exports = async (onEnd = false) => {
         delete exerciseCopy[oldField];
         exerciseCopy[newField] = exercise[oldField];
       });
+      if (['Body Only', 'Other'].includes(exerciseCopy.equipment)) {
+        exerciseCopy.equipment = null;
+      }
+      if (exerciseCopy.rating === 'n/a') {
+        exerciseCopy.rating = null;
+      }
       return exerciseCopy;
     })
   );
+
+  console.log('Seeding workouts collection...');
+  await Workout.create(workouts);
+
+  console.log('Seeding workout_exercises collection...');
+  const workoutExercises = [
+    {
+      workoutId: (await Workout.findOne()).id,
+      exerciseId: (await Exercise.findOne({ muscle: 'Chest' })).id,
+      time: 30
+    },
+    {
+      workoutId: (await Workout.findOne()).id,
+      exerciseId: (await Exercise.findOne({ muscle: 'Lats' })).id,
+      time: 30
+    }
+  ];
+  await WorkoutExercise.create(workoutExercises);
 
   console.log('Successfully seeded the database!');
   return onEnd ? onEnd() : true;
