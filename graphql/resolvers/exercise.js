c5b9f99cf273e5c94111d413b59ecf18983523c3
Leelsmuth <femianjorin@rocketmail.com>
2020-01-08 13:23:27 +0100

    implemented find exercise by id

--- graphql/resolvers/exercise.js
+++ graphql/resolvers/exercise.js
@@ -5,6 +5,10 @@ module.exports = {
     exercises: async () => {
       const exercises = await Exercise.find();
       return exercises;
+    },
+    exercise: async (_, { id }) => {
+      const exercise = await Exercise.findById(id);
+      return { ...exercise._doc, id: exercise.id };
     }
   },
   Mutation: {}
