a881c408da15abf90f327bcea4d1bf1f2781904e
Melquisedeque Pereira <melqui1@live.com.pt>
2020-01-14 08:46:49 +0000

    Removed cors from apollo server options

--- api/server.js
+++ api/server.js
@@ -23,9 +23,6 @@ app.get('/', (req, res) => {
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 const schemaWithMiddleware = applyMiddleware(schema, validators, permissions);
 const apolloServer = new ApolloServer({
-  cors: {
-    origin: '*',
-  },
   schema: schemaWithMiddleware,
   context,
   introspection: graphiql,
