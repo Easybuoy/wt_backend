4696824ab2feaa9fa344e478aac13758d27a0bc8
Leelsmuth <femianjorin@rocketmail.com>
2020-01-22 17:16:49 +0100

    removed commented code

--- graphql/resolvers/__tests/exercise.spec.js
+++ graphql/resolvers/__tests/exercise.spec.js
@@ -6,18 +6,12 @@ initTest();
 describe('exercise', () => {
   it('it should fetch all exercises', async (done) => {
     // eslint-disable-next-line no-undef
-    const exercises = Exercise.Query.exercises(null, { search: '', fields: '' });
+    const exercises = Exercise.Query.exercises(null, {
+      search: '',
+      fields: ''
+    });
     expect(exercises).toBeDefined();
     done();
   });
   jest.setTimeout(30000);
-
-//   it('it should fetch a single exercise', async (done) => {
-//     // eslint-disable-next-line no-undef
-//     const id = '5e25e3385ee7785a404e68b0';
-//     const exercise = await Exercise.Query.exercise(null, { id });
-//     expect(exercise).toBeFalsy();
-//     done();
-//   });
-//   jest.setTimeout(30000);
 });
